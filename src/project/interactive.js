17dcae01e4c217e087e42ef4a2ac29c84b61abda
Apurbopaul131 <apurbopaul131@gmail.com>
2024-09-03 12:20:00 +0600

    Coplete all features with area callculation and add the list item

--- src/project/interactive.js
+++ src/project/interactive.js
@@ -1,88 +1,3 @@
-//Add onclick handler to the callculate button of the Triangle
-document.getElementById("btn-triangle").addEventListener("click", function () {
-  // Extact base and height by clling function
-  const triangleBase = takeInputValueById("triangle-base");
-  const triangleHeight = takeInputValueById("triangle-height");
-
-  //callculate area of the triangle
-  const areaTriangle = callculateTriangleOrPentagonArea(
-    triangleBase,
-    triangleHeight
-  );
-  console.log(areaTriangle);
-
-  //set the result
-  const triangleTitle = document.getElementById("triangle-title").innerText;
-  setInnerHtml(areaTriangle, triangleTitle);
-});
-
-//Add onclick handler to the callculate button of the rectangle
-document.getElementById("btn-rectangle").addEventListener("click", function () {
-  // Extract length and width of rectangle from input field
-  const rectangleLen = takeInputValueById("rectangle-length");
-  const rectanglewidth = takeInputValueById("rectangle-width");
-
-  //callculate area of the rectangle
-  const areaRectangle = callculateRectangleArea(rectangleLen, rectanglewidth);
-  console.log(areaRectangle);
-
-  //Set the result
-  const rectangleTitle = document.getElementById("rectangle-title").innerText;
-  setInnerHtml(areaRectangle, rectangleTitle);
-});
-
-//Add onclick handler to the callculate button of the Parallelogram
-document
-  .getElementById("btn-parallelogram")
-  .addEventListener("click", function () {
-    //Extract Base and height of rectangle from input field
-    const parallelogramBase = takeInputValueById("parallelogram-base");
-    const parallelogramHeight = takeInputValueById("parallelogram-height");
-
-    //callculate area of the parallelogram
-    const areaParallelogram = callculateParallelogramOrRhombusArea(
-      parallelogramBase,
-      parallelogramHeight
-    );
-    console.log(areaParallelogram);
-  });
-
-//Add onclick handler to the callculate button of the Rhombus
-document.getElementById("btn-rhombus").addEventListener("click", function () {
-  //Extract Base and height of rectangle from input field
-  const rhombusBase = takeInputValueById("rhombus-base");
-  const rhombusHeight = takeInputValueById("rhombus-height");
-
-  //callculate area of the rhombus
-  const areaRhombus = callculateParallelogramOrRhombusArea(
-    rhombusBase,
-    rhombusHeight
-  );
-  console.log(areaRhombus);
-});
-
-//Add onclick handler to the callculate button of the Pentagon
-document.getElementById("btn-pentagon").addEventListener("click", function () {
-  //Extract Base and height of rectangle from input field
-  const pentagonPerimeter = takeInputValueById("pentagon-perimeter");
-  const pentagonApothem = takeInputValueById("pentagon-apothem");
-
-  //callculate area of the pentagon
-  const areaPentagon = callculateTriangleOrPentagonArea(
-    pentagonPerimeter,
-    pentagonApothem
-  );
-  console.log(areaPentagon);
-});
-
-//Add onclick handler to the callculate button of the Ellipse
-document.getElementById("btn-ellipse").addEventListener("click", function () {
-  const ellipseBase = takeInputValueById("ellipse-base");
-  const ellipseHeight = takeInputValueById("ellipse-height");
-  const areaEllipse = callculateEllipseArea(ellipseBase, ellipseHeight);
-  console.log(areaEllipse.toFixed(2));
-});
-
 // This function take id as a parameter and extract value from the input field
 const takeInputValueById = (inputFeildId) => {
   const inputElement = document.getElementById(inputFeildId);
@@ -130,10 +45,14 @@ const setInnerHtml = (area, title) => {
   // Create area span
   const areaSpan = document.createElement("span");
   areaSpan.innerHTML = `${area}cm<sup>2</sup>`;
+  areaSpan.id = `${title}-field`;
 
   // Create button
   const button = document.createElement("button");
   button.className = "btn btn-primary btn-sm";
+
+  //add button id
+  button.id = `${title}-meter`;
 
   // Create button text
   const buttonText = document.createElement("span");
@@ -150,3 +69,195 @@ const setInnerHtml = (area, title) => {
   // Append the list item to the ordered list
   targetElement.appendChild(li);
 };
+
+//This function used for print error massage
+const printErrorMassage = (massageFieldId) => {
+  const errorMassage = document.getElementById(massageFieldId);
+  errorMassage.innerText = "input not valid...";
+  errorMassage.className = "text-red-500";
+};
+
+// This function used for clear input filed
+const clearInputField = (fieldId) => {
+  document.getElementById(fieldId).value = "";
+};
+
+//Add onclick handler to the callculate button of the Triangle
+document.getElementById("btn-triangle").addEventListener("click", function () {
+  // Extact base and height by clling function
+  const triangleBase = takeInputValueById("triangle-base");
+  const triangleHeight = takeInputValueById("triangle-height");
+  if (
+    Number.isNaN(triangleHeight) === false &&
+    Number.isNaN(triangleBase) === false
+  ) {
+    // clear invalid massage
+    document.getElementById("triangle-error").innerText = "";
+
+    //callculate area of the triangle
+    const areaTriangle = callculateTriangleOrPentagonArea(
+      triangleBase,
+      triangleHeight
+    );
+    console.log(areaTriangle);
+
+    //set the result
+    const triangleTitle = document.getElementById("triangle-title").innerText;
+    setInnerHtml(areaTriangle, triangleTitle);
+
+    //clear input filed
+    clearInputField("triangle-base");
+    clearInputField("triangle-height");
+  } else {
+    printErrorMassage("triangle-error");
+  }
+});
+
+//Add onclick handler to the callculate button of the rectangle
+document.getElementById("btn-rectangle").addEventListener("click", function () {
+  // Extract length and width of rectangle from input field
+  const rectangleLen = takeInputValueById("rectangle-length");
+  const rectanglewidth = takeInputValueById("rectangle-width");
+  if (
+    Number.isNaN(rectangleLen) === false &&
+    Number.isNaN(rectanglewidth) === false
+  ) {
+    //callculate area of the rectangle
+    const areaRectangle = callculateRectangleArea(rectangleLen, rectanglewidth);
+    console.log(areaRectangle);
+
+    //Set the result
+    const rectangleTitle = document.getElementById("rectangle-title").innerText;
+    setInnerHtml(areaRectangle, rectangleTitle);
+
+    clearInputField("rectangle-length");
+    clearInputField("rectangle-width");
+  } else {
+    printErrorMassage("rectangle-error");
+  }
+});
+
+//Add onclick handler to the callculate button of the Parallelogram
+document
+  .getElementById("btn-parallelogram")
+  .addEventListener("click", function () {
+    //Extract Base and height of rectangle from input field
+    const parallelogramBase = takeInputValueById("parallelogram-base");
+    const parallelogramHeight = takeInputValueById("parallelogram-height");
+    if (
+      Number.isNaN(parallelogramBase) === false &&
+      Number.isNaN(parallelogramHeight) === false
+    ) {
+      // clear error massage
+      document.getElementById("parallelogram-error").innerText = "";
+
+      //callculate area of the parallelogram
+      const areaParallelogram = callculateParallelogramOrRhombusArea(
+        parallelogramBase,
+        parallelogramHeight
+      );
+      console.log(areaParallelogram);
+
+      //set the result
+      const parallelogramTitle = document.getElementById(
+        "parallelogram-title"
+      ).innerText;
+      setInnerHtml(areaParallelogram, parallelogramTitle);
+
+      //clear the input field of the parallelogram
+      clearInputField("parallelogram-base");
+      clearInputField("parallelogram-height");
+    } else {
+      printErrorMassage("parallelogram-error");
+    }
+  });
+
+//Add onclick handler to the callculate button of the Rhombus
+document.getElementById("btn-rhombus").addEventListener("click", function () {
+  //Extract Base and height of rectangle from input field
+  const rhombusBase = takeInputValueById("rhombus-base");
+  const rhombusHeight = takeInputValueById("rhombus-height");
+
+  if (
+    Number.isNaN(rhombusBase) === false &&
+    Number.isNaN(rhombusHeight) === false
+  ) {
+    //clear error massage
+    document.getElementById("rhombus-error").innerText = "";
+
+    //callculate area of the rhombus
+    const areaRhombus = callculateParallelogramOrRhombusArea(
+      rhombusBase,
+      rhombusHeight
+    );
+    console.log(areaRhombus);
+
+    //set result
+    const rhombusTitle = document.getElementById("rhombus-title").innerText;
+    setInnerHtml(areaRhombus, rhombusTitle);
+
+    //clear input filed
+    clearInputField("rhombus-base");
+    clearInputField("rhombus-height");
+  } else {
+    printErrorMassage("rhombus-error");
+  }
+});
+
+//Add onclick handler to the callculate button of the Pentagon
+document.getElementById("btn-pentagon").addEventListener("click", function () {
+  //Extract Base and height of rectangle from input field
+  const pentagonPerimeter = takeInputValueById("pentagon-perimeter");
+  const pentagonApothem = takeInputValueById("pentagon-apothem");
+
+  if (
+    Number.isNaN(pentagonPerimeter) === false &&
+    Number.isNaN(pentagonApothem) === false
+  ) {
+    // clear error massage
+    document.getElementById("pentagon-error").innerText = "";
+    //callculate area of the pentagon
+    const areaPentagon = callculateTriangleOrPentagonArea(
+      pentagonPerimeter,
+      pentagonApothem
+    );
+    console.log(areaPentagon);
+
+    //set result
+    const pentagonTitle = document.getElementById("pentagon-title").innerText;
+    setInnerHtml(areaPentagon, pentagonTitle);
+
+    //clear input field
+    clearInputField("pentagon-perimeter");
+    clearInputField("pentagon-apothem");
+  } else {
+    printErrorMassage("pentagon-error");
+  }
+});
+
+//Add onclick handler to the callculate button of the Ellipse
+document.getElementById("btn-ellipse").addEventListener("click", function () {
+  const ellipseBase = takeInputValueById("ellipse-base");
+  const ellipseHeight = takeInputValueById("ellipse-height");
+  if (
+    Number.isNaN(ellipseBase) === false &&
+    Number.isNaN(ellipseHeight) === false
+  ) {
+    //clear error massage
+    document.getElementById("ellipse-error").innerText = "";
+
+    //callculate area of the ellipse
+    const areaEllipse = callculateEllipseArea(ellipseBase, ellipseHeight);
+    console.log(areaEllipse.toFixed(2));
+
+    //set result
+    const ellipseTitle = document.getElementById("ellipse-title").innerText;
+    setInnerHtml(areaEllipse.toFixed(2), ellipseTitle);
+
+    //clear input field
+    clearInputField("ellipse-base");
+    clearInputField("ellipse-height");
+  } else {
+    printErrorMassage("ellipse-error");
+  }
+});
